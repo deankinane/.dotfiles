1c0f5ea7b8d477daa115254065e375432bcd8da2
Dean Kinane <dean.kinane@gmail.com>
2022-12-12 13:04:37 +0000

    Added vim alias for nvim

--- .config/nvim/lua/deank/packer.lua
+++ .config/nvim/lua/deank/packer.lua
@@ -4,12 +4,12 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'folke/tokyonight.nvim'
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.0',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use 'folke/tokyonight.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 end)
