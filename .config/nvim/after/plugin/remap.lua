9edc38ed19adb692a29566f09735def7bc8ddeac
Dean Kinane <dean.kinane@gmail.com>
2022-12-22 14:10:38 +0000

    Added system clipboard support for WSL. Added new keybinds

--- .config/nvim/after/plugin/remap.lua
+++ .config/nvim/after/plugin/remap.lua
@@ -1,21 +1,42 @@
+local map = vim.keymap.set
+
 -- Harpoon
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 local term = require('harpoon.term')
 
-vim.keymap.set('n', '<C-a>',function() mark.add_file() end)
-vim.keymap.set('n', '<C-e>', function() ui.toggle_quick_menu() end)
-vim.keymap.set('n', '<leader>h', function() ui.nav_file(1) end)
-vim.keymap.set('n', '<leader>j', function() ui.nav_file(2) end)
-vim.keymap.set('n', '<leader>k', function() ui.nav_file(3) end)
-vim.keymap.set('n', '<leader>l', function() ui.nav_file(4) end)
-vim.keymap.set('n', '<leader>1', function() term.gotoTerminal(1) end)
-vim.keymap.set('n', '<leader>2', function() term.gotoTerminal(2) end)
-vim.keymap.set('n', '<leader>3', function() term.gotoTerminal(3) end)
-vim.keymap.set('n', '<leader>4', function() term.gotoTerminal(4) end)
+map('n', '<C-a>',function() mark.add_file() end)
+map('n', '<C-e>', function() ui.toggle_quick_menu() end)
+map('n', '<leader>h', function() ui.nav_file(1) end)
+map('n', '<leader>j', function() ui.nav_file(2) end)
+map('n', '<leader>k', function() ui.nav_file(3) end)
+map('n', '<leader>l', function() ui.nav_file(4) end)
+map('n', '<leader>1', function() term.gotoTerminal(1) end)
+map('n', '<leader>2', function() term.gotoTerminal(2) end)
+map('n', '<leader>3', function() term.gotoTerminal(3) end)
+map('n', '<leader>4', function() term.gotoTerminal(4) end)
 
 -- Neovim
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>w', vim.cmd.write)
-vim.keymap.set('t', '<leader><ESC>', '<C-\\><C-n>')
-vim.keymap.set('n', '<leader><leader>x', vim.cmd.so)
+map('n', '<leader>pv', vim.cmd.Ex)
+map('n', '<leader>w', vim.cmd.write)
+map('t', '<leader><ESC>', '<C-\\><C-n>')
+map('n', '<leader><leader>x', vim.cmd.so)
+map('n', '<leader>c', ':bd')
+
+map("v", "J", ":m '>+1<CR>gv=gv") -- Move selection up and down
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("n", "<C-d>", "<C-d>zz") -- Half page jumping
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv") -- Keep search results in middle of screen
+map("n", "N", "Nzzzv")
+
+map("x", "<leader>p", [["_dP]]) -- Paste over selection while preserving register
+
+map({"n", "v"}, "<leader>y", [["+y]]) -- Copy to system clipboard
+map("n", "<leader>Y", [["+Y]])
+
+map({"n", "v"}, "<leader>d", [["_d]]) -- Delete to void register
+
+map("n", "Q", "<nop>")
+
--- .config/nvim/after/plugin/set.lua
+++ .config/nvim/after/plugin/set.lua
@@ -21,3 +21,16 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 50
 
 --vim.opt.colorcolumn = "80"
+
+vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+        ['+']= 'clip.exe',
+        ['*']= 'clip.exe',
+    },
+    paste = {
+        ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0
+}
